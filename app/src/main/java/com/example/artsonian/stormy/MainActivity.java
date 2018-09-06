4d461c1a4b3fee2813c0fc74eea8bc95e2d5d44a
syoustra <steph.you07@gmail.com>
2018-09-06 13:33:34 -0400

    Set up OkHttp call

--- app/src/main/java/com/example/artsonian/stormy/MainActivity.java
+++ app/src/main/java/com/example/artsonian/stormy/MainActivity.java
@@ -2,12 +2,67 @@ package com.example.artsonian.stormy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String apiKey = "a69891e750ac013263b9570f6c65e5ac";
+
+        double latitude = 37.8267;
+        double longitude = -122.4233;
+
+        String forecastURL = "https://api.darksky.net/forecast/"
+                + apiKey + "/" + latitude + "," + longitude;
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(forecastURL)
+                .build();
+
+        Call call = client.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                try {
+                    Log.v(TAG, response.body().string());
+                    if (response.isSuccessful()) {
+
+                    }
+                    else {
+                        alertUserAboutError();
+                    }
+
+                } catch (IOException e) {
+                    Log.e(TAG, "IO Exception caught: ", e);
+                }
+
+            }
+        });
+        Log.d(TAG, "Main UI code is running");
+    }
+
+    private void alertUserAboutError() {
+
     }
 }
