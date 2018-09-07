4bf31e51f018925c7e6ee9469d34bd43cbb7a99d
syoustra <steph.you07@gmail.com>
2018-09-07 16:01:51 -0400

    Update weather icon

--- app/src/main/java/com/example/artsonian/stormy/MainActivity.java
+++ app/src/main/java/com/example/artsonian/stormy/MainActivity.java
@@ -2,12 +2,14 @@ package com.example.artsonian.stormy;
 
 import android.content.Context;
 import android.databinding.DataBindingUtil;
+import android.graphics.drawable.Drawable;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.method.LinkMovementMethod;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,6 +32,8 @@ public class MainActivity extends AppCompatActivity {
 
     private CurrentWeather currentWeather;
 
+    private ImageView iconImageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,8 +41,9 @@ public class MainActivity extends AppCompatActivity {
                 R.layout.activity_main);
 
         TextView darkSky = findViewById(R.id.darkSkyAttribution);
-
         darkSky.setMovementMethod(LinkMovementMethod.getInstance());
+
+        iconImageView = findViewById(R.id.iconImageView);
 
         String apiKey = "a69891e750ac013263b9570f6c65e5ac";
 
@@ -71,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
 
                             currentWeather = getCurrentDetails(jsonData);
 
-                            CurrentWeather displayWeather = new CurrentWeather(
+                            final CurrentWeather displayWeather = new CurrentWeather(
                                     currentWeather.getLocationLabel(),
                                     currentWeather.getIcon(),
                                     currentWeather.getTime(),
@@ -83,6 +88,15 @@ public class MainActivity extends AppCompatActivity {
                             );
 
                             binding.setWeather(displayWeather);
+
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    Drawable drawable = getResources().getDrawable(displayWeather.getIconId());
+                                    iconImageView.setImageDrawable(drawable);
+                                }
+                            });
+
 
                         } else {
                             alertUserAboutError();
