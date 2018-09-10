a19fe2ab4dee1b5fb11f1a5357c8055444920bb6
syoustra <steph.you07@gmail.com>
2018-09-09 21:47:59 -0400

    Fix time formatting and connect weather icons in Hourly view

--- app/src/main/java/com/example/artsonian/stormy/weather/Forecast.java
+++ app/src/main/java/com/example/artsonian/stormy/weather/Forecast.java
@@ -1,5 +1,7 @@
 package com.example.artsonian.stormy.weather;
 
+import com.example.artsonian.stormy.R;
+
 public class Forecast {
 
     private Current current;
@@ -19,5 +21,44 @@ public class Forecast {
 
     public void setHourlyForecast(Hour[] hourlyForecast) {
         this.hourlyForecast = hourlyForecast;
+    }
+
+    public static int getIconId(String iconString) {
+        int iconId = R.drawable.clear_day;
+
+        switch(iconString) {
+            case "clear-day":
+                iconId = R.drawable.clear_day;
+                break;
+            case "clear-night":
+                iconId = R.drawable.clear_night;
+                break;
+            case "rain":
+                iconId = R.drawable.rain;
+                break;
+            case "snow":
+                iconId = R.drawable.snow;
+                break;
+            case "sleet":
+                iconId = R.drawable.sleet;
+                break;
+            case "wind":
+                iconId = R.drawable.wind;
+                break;
+            case "fog":
+                iconId = R.drawable.fog;
+                break;
+            case "cloudy":
+                iconId = R.drawable.cloudy;
+                break;
+            case "partly-cloudy-day":
+                iconId = R.drawable.partly_cloudy;
+                break;
+            case "partly-cloudy-night":
+                iconId = R.drawable.cloudy_night;
+                break;
+        }
+        return iconId;
+
     }
 }
--- app/src/main/java/com/example/artsonian/stormy/weather/Hour.java
+++ app/src/main/java/com/example/artsonian/stormy/weather/Hour.java
@@ -1,5 +1,9 @@
 package com.example.artsonian.stormy.weather;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 public class Hour {
 
     private long time;
@@ -19,8 +23,12 @@ public class Hour {
         this.timeZone = timeZone;
     }
 
-    public long getTime() {
-        return time;
+    public String getTime() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h a");
+        formatter.setTimeZone(TimeZone.getTimeZone(timeZone));
+
+        Date dateTime = new Date(time * 1000);
+        return formatter.format(dateTime);
     }
 
     public void setTime(long time) {
@@ -35,16 +43,16 @@ public class Hour {
         this.summary = summary;
     }
 
-    public double getTemperature() {
-        return temperature;
+    public int getTemperature() {
+        return (int) Math.round(temperature);
     }
 
     public void setTemperature(double temperature) {
         this.temperature = temperature;
     }
 
-    public String getIcon() {
-        return icon;
+    public int getIcon() {
+        return Forecast.getIconId(icon);
     }
 
     public void setIcon(String icon) {
